0a1dc31d847bd85333d9e1274046acc0e4c7f510
Jacob Delott <jldelott@gmail.com>
2023-11-07 10:36:26 -0800

    reduced skills for mobile testing

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -45,16 +45,16 @@ const Skills = () => {
 
         <Skill name="HTML" x="-20vw" y="2vw" />
 
-          <Skill name="CSS" x="-5vw" y="-10vw" />
+          {/* <Skill name="CSS" x="-5vw" y="-10vw" /> */}
           <Skill name="JavaScript" x="20vw" y="6vw" />
           <Skill name="ReactJS" x="0vw" y="12vw" />
           <Skill name="NextJS" x="-20vw" y="-15vw" />
           <Skill name="Python" x="-20vw" y="-15vw" />
-          <Skill name="Docker" x="-20vw" y="-15vw" />
+          {/* <Skill name="Docker" x="-20vw" y="-15vw" /> */}
           <Skill name="FastAPI" x="-20vw" y="-15vw" />
 
           {/* <Skill name="GatsbyJS" x="15vw" y="-12vw" /> */}
-          <Skill name="Github" x="-35vw" y="-5vw" />
+          {/* <Skill name="Github" x="-35vw" y="-5vw" /> */}
           <Skill name="PostgreSQL" x="32vw" y="-5vw" />
           <Skill name="Django" x="0vw" y="-20vw" />
           {/* <Skill name="Firebase" x="-25vw" y="18vw" /> */}
